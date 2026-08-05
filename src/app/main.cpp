f1e8e0eeb63ae9a6853aafeb027d597b61732469
hanslhansl <>
2026-08-06 00:35:05 +0200

    a

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,56 +1,38 @@
-﻿
+﻿import std;
 
-import erdo.ui;
-int main(int argc, char *argv[])
-{
-    return erdo::ui::run_ui(argc, argv);
-}
+//import erdo.ui;
+//int main(int argc, char *argv[])
+//{
+//    return erdo::ui::run_ui(argc, argv);
+//}
 
-
-// import erdo;
-// int main (int argc, char *argv[])
-// {
-//     erdo::witchy::run_witchy(
-//         ".../Steam/steamapps/common/ELDEN RING/Game",
-//         ".../WitchyBND-v3.0.0.1-win-x64/WitchyBND.exe",
-//         ".../elden-ring-damage-optimizer/xml_data"
-//     );
-
-//     return 0;
-// }
-
-
-
-/*
+#include <tracy/Tracy.hpp>
 import erdo;
-import BS.thread_pool;
 
 int main(int argc, char* argv[])
 {
     auto executable_path = std::filesystem::absolute(std::filesystem::path(argv[0]));
-    auto xml_data_directory = executable_path.parent_path() / "xml_data";
+    auto xml_data_directory = executable_path.parent_path() / "xml_data" / "11611000";
 
-    auto weapons = xml::load_weapons(xml_data_directory);
+    auto w = erdo::parser::load_weapons(xml_data_directory);
+    std::vector<std::reference_wrapper<const erdo::calculator::Weapon>> weapons{ std::from_range, w };
 
-    auto stat_variations = calculator::get_stat_variations(
-        1 + 60,
-        calculator::character_class_stats.at("wretch").to_stats()
-    );
+    // while(running)
+    {
+        FrameMark;
+        auto stat_variations = erdo::calculator::get_stat_variations(
+            120,
+            erdo::calculator::character_class_stats.at("wretch")
+        );
+        erdo::calculator::AttackOptions attack_options{ {0, 25, 10}, true };
 
-    calculator::AttackOptions attack_options{ {0, 25, 10}, true };
-    BS::thread_pool<> thread_pool{ 1 };
-    std::vector<calculator::AttackRating> attack_ratings = optimizer::optimize<optimizer::optimize_weapon>(weapons, stat_variations, attack_options, thread_pool).get();
-    auto&& attack_rating = attack_ratings.front();
+        constexpr auto optimizer = erdo::calculator::optimizers<erdo::calculator::OptimizationTarget::TOTAL_ATTACK_POWER>;
 
-    std::println("{}", attack_rating.weapon.get().full_name);
+        volatile auto attack_ratings = optimizer.run_synchronously(weapons, stat_variations, attack_options);
+    }
 
-    //calculator::AttackOptions attack_options{{0, 25, 10}, true};
-    //calculator::Stats stats{ 21, 10, 10, 10, 10 };
-
-    //auto total_attack_powers = weapons | std::views::transform([&](const calculator::Weapon& w){
-    //    return w.get_attack_rating(attack_options, stats).total_attack_power.at(1);
-    //}) | std::ranges::to<std::vector>();
-    //std::println("{}", total_attack_powers);
+    
+    
 
     return 0;
-}*/
+}
